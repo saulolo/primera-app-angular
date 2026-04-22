d9c0a2bcb5555608340badeb9524d0dde9307aa1
saulolo <saulolo@gmail.com>
2026-04-22 16:26:14 -0500

    🎉 feat(ANGULAR-USUARIO): Refactoriza componente para recibir objeto usuario y simplifica renderizado dinámico

--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -1,5 +1,17 @@
 import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  nombre: string;
+}
+
+/*type User = {
+  id: string;
+  avatar: string;
+  nombre: string;
+}*/
+
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.html',
@@ -7,14 +19,14 @@ import { Component, computed, EventEmitter, Input, input, Output, output } from 
 })
 export class Usuario {
   //Con decorador Input
-  @Input({ required: true }) id!:string;
+  @Input({ required: true }) usuario!: User;
   //@Input({ required: true }) avatar!: string;
   //@Input({ required: true }) nombre!: string;
- // @Output() seleccion = new EventEmitter<string>();
+  //@Output() seleccion = new EventEmitter<string>();
 
   //Con Signals
-  avatar = input.required<string>();
-  nombre = input.required<string>();
+  //avatar = input.required<string>();
+  //nombre = input.required<string>();
   seleccion = output<string>();
 
   /*  get rutaImagen() {
@@ -22,10 +34,10 @@ export class Usuario {
   }*/
 
   rutaImagen = computed(() => {
-    return `/assets/usuarios/${this.avatar()}`;
+    return `/assets/usuarios/${this.usuario.avatar}`;
   });
 
   alSeleccionarUsuario() {
-    this.seleccion.emit(this.id);
+    this.seleccion.emit(this.usuario.id);
   }
 }
